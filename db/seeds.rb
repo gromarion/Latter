83a972d1ff6a34a2ef82ec55b18208ae94e04d83
German Rodrigo Romarion <grromarion90@gmail.com>
2015-08-30 16:09:34 -0300

    Added new Badges.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,37 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Badge.create(name: 'Silver Bull', image_url: "https://s3.amazonaws.com/f.cl.ly/items/0n3N252J1q1l333B2o3W/price-silver.png", description: "Get over 1001 points", required_rating: 1001)
-Badge.create(name: 'Gold Bull', image_url: "https://s3.amazonaws.com/f.cl.ly/items/0v1Z0p230I2j1T402K0P/price-gold.png", description: "Get over 1020 points", required_rating: 1020)
+Badge.create(
+  name: 'Break the Ice',
+  image_url: 'https://s3.amazonaws.com/f.cl.ly/items/0n3N252J1q1l333B2o3W/price-silver.png',
+  description: 'Get over 1001 points',
+  required_rating: 1001
+)
+
+Badge.create(
+  name: 'Bronze',
+  image_url: 'https://s3.amazonaws.com/f.cl.ly/items/0v1Z0p230I2j1T402K0P/price-gold.png',
+  description: 'Get over 1100 points',
+  required_rating: 1100
+)
+
+Badge.create(
+  name: 'Silver',
+  image_url: 'https://s3.amazonaws.com/f.cl.ly/items/0v1Z0p230I2j1T402K0P/price-gold.png',
+  description: 'Get over 1500 points',
+  required_rating: 1500
+)
+
+Badge.create(
+  name: 'Gold',
+  image_url: 'https://s3.amazonaws.com/f.cl.ly/items/0v1Z0p230I2j1T402K0P/price-gold.png',
+  description: 'Get over 2000 points',
+  required_rating: 2000
+)
+
+Badge.create(
+  name: 'Platinum',
+  image_url: 'https://s3.amazonaws.com/f.cl.ly/items/0v1Z0p230I2j1T402K0P/price-gold.png',
+  description: 'Get over 2500 points',
+  required_rating: 2500
+)
