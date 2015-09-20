f53a2f4bb3a2d0c8fde8e18eb4080aacb6fd9f2d
German Rodrigo Romarion <grromarion90@gmail.com>
2015-09-19 21:12:45 -0300

    Implemented badge sorting.

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -1,6 +1,10 @@
 class BadgesController < ApplicationController
+
+  BADGES_ORDER = ['Break the Ice', 'Bronze', 'Silver', 'Gold', 'Platinum']
+
   def index
     @badges = Badge.all
+    @badges = @badges.sort { |x, y| x.name <=> y.name }
     render stream: true
   end
 
